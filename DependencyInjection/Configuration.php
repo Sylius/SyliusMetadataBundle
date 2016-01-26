pamil/Sylius

[Metadata] Added base page metadata model and forms for metadatas

src/Sylius/Bundle/SeoBundle/DependencyInjection/Configuration.php
@@ -60,6 +60,26 @@ class Configuration implements ConfigurationInterface
                             ->prototype('scalar')->end()
                             ->defaultValue(array('sylius'))
                         ->end()
+                        ->arrayNode('page_metadata')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_summary_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_summary_large_image_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_player_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_app_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
                     ->end()
                 ->end()
             ->end()
@@ -82,13 +102,42 @@ class Configuration implements ConfigurationInterface
                                 ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\RootMetadata')->end()
                                 ->scalarNode('controller')->defaultValue('Sylius\Bundle\ResourceBundle\Controller\ResourceController')->end()
                                 ->scalarNode('repository')->end()
-                                    ->arrayNode('form')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('default')->end()
-                                        ->scalarNode('choice')->end()
-                                    ->end()
-                                ->end()
+                                ->scalarNode('form')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('page_metadata')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\Custom\PageMetadata')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\SeoBundle\Form\Type\Custom\PageMetadataType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_summary_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\Twitter\SummaryCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\SeoBundle\Form\Type\Twitter\SummaryCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_summary_large_image_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\Twitter\SummaryLargeImageCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\SeoBundle\Form\Type\Twitter\SummaryLargeImageCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_player_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\Twitter\PlayerCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\SeoBundle\Form\Type\Twitter\PlayerCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_app_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Seo\Model\Twitter\AppCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\SeoBundle\Form\Type\Twitter\AppCardType')->end()
                             ->end()
                         ->end()
                     ->end()

src/Sylius/Bundle/SeoBundle/Form/Type/Twitter/AppCardType.php
@@ -1,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SeoBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class AppCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('description', 'textarea')
+            ->add('appNameIphone', 'text')
+            ->add('appIdIphone', 'text')
+            ->add('appUrlIphone', 'text')
+            ->add('appNameIpad', 'text')
+            ->add('appIdIpad', 'text')
+            ->add('appUrlIpad', 'text')
+            ->add('appNameGooglePlay', 'text')
+            ->add('appIdGooglePlay', 'text')
+            ->add('appUrlGooglePlay', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_app_card';
+    }
+}

src/Sylius/Bundle/SeoBundle/Form/Type/Twitter/PlayerCardType.php
@@ -1,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SeoBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class PlayerCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+            ->add('player', 'textarea')
+            ->add('playerWidth', 'number')
+            ->add('playerHeight', 'number')
+            ->add('playerStream', 'text')
+            ->add('playerStreamContentType', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_player_card';
+    }
+}

src/Sylius/Bundle/SeoBundle/Form/Type/Twitter/SummaryCardType.php
@@ -1,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SeoBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('creatorId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_summary_card';
+    }
+}

src/Sylius/Bundle/SeoBundle/Form/Type/Twitter/SummaryLargeImageCardType.php
@@ -1,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\SeoBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryLargeImageCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('creator', 'text')
+            ->add('creatorId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_summary_large_image_card';
+    }
+}

Sylius/SyliusMetadataBundle

DependencyInjection/Configuration.php
@@ -60,6 +60,26 @@ class Configuration implements ConfigurationInterface
                             ->prototype('scalar')->end()
                             ->defaultValue(array('sylius'))
                         ->end()
+                        ->arrayNode('page_metadata')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_summary_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_summary_large_image_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_player_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
+                        ->arrayNode('twitter_app_card')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(array('sylius'))
+                        ->end()
                     ->end()
                 ->end()
             ->end()
@@ -82,13 +102,42 @@ class Configuration implements ConfigurationInterface
                                 ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\RootMetadata')->end()
                                 ->scalarNode('controller')->defaultValue('Sylius\Bundle\ResourceBundle\Controller\ResourceController')->end()
                                 ->scalarNode('repository')->end()
-                                    ->arrayNode('form')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('default')->end()
-                                        ->scalarNode('choice')->end()
-                                    ->end()
-                                ->end()
+                                ->scalarNode('form')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('page_metadata')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\Custom\PageMetadata')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\MetadataBundle\Form\Type\Custom\PageMetadataType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_summary_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\Twitter\SummaryCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\MetadataBundle\Form\Type\Twitter\SummaryCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_summary_large_image_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\Twitter\SummaryLargeImageCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\MetadataBundle\Form\Type\Twitter\SummaryLargeImageCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_player_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\Twitter\PlayerCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\MetadataBundle\Form\Type\Twitter\PlayerCardType')->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('twitter_app_card')
+                        ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('model')->defaultValue('Sylius\Component\Metadata\Model\Twitter\AppCard')->end()
+                                ->scalarNode('form')->defaultValue('Sylius\Bundle\MetadataBundle\Form\Type\Twitter\AppCardType')->end()
                             ->end()
                         ->end()
                     ->end()

Form/Type/Twitter/AppCardType.php
@@ -1,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\MetadataBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class AppCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('description', 'textarea')
+            ->add('appNameIphone', 'text')
+            ->add('appIdIphone', 'text')
+            ->add('appUrlIphone', 'text')
+            ->add('appNameIpad', 'text')
+            ->add('appIdIpad', 'text')
+            ->add('appUrlIpad', 'text')
+            ->add('appNameGooglePlay', 'text')
+            ->add('appIdGooglePlay', 'text')
+            ->add('appUrlGooglePlay', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_app_card';
+    }
+}

Form/Type/Twitter/PlayerCardType.php
@@ -1,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\MetadataBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class PlayerCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+            ->add('player', 'textarea')
+            ->add('playerWidth', 'number')
+            ->add('playerHeight', 'number')
+            ->add('playerStream', 'text')
+            ->add('playerStreamContentType', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_player_card';
+    }
+}

Form/Type/Twitter/SummaryCardType.php
@@ -1,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\MetadataBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('creatorId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_summary_card';
+    }
+}

Form/Type/Twitter/SummaryLargeImageCardType.php
@@ -1,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\MetadataBundle\Form\Type\Twitter;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryLargeImageCardType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('site', 'text')
+            ->add('siteId', 'text')
+            ->add('creator', 'text')
+            ->add('creatorId', 'text')
+            ->add('title', 'text')
+            ->add('description', 'textarea')
+            ->add('image', 'text')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_twitter_summary_large_image_card';
+    }
+}